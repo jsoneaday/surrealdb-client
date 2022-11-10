f1146bd740a91cd4405d306d8198cdcad36192c3
David Choi <dharric@live.com>
2022-11-10 13:14:30 -0500

    rename test functions

--- tests/surreal_ws_conn_test.rs
+++ tests/surreal_ws_conn_test.rs
@@ -1,9 +1,9 @@
 mod common;
 
-use common::{ FIXTURES };
+use common::FIXTURES;
 
 #[tokio::test]
-async fn int_verify_connect_makes_connection_to_surrealdb() {       
+async fn verify_connect_makes_connection_to_surrealdb() {       
     let mut fixture = FIXTURES.lock().unwrap();
     let fixture_items = fixture.instance.as_mut().unwrap();
     let result = fixture_items.conn.connect().await;
@@ -13,11 +13,11 @@ async fn int_verify_connect_makes_connection_to_surrealdb() {
 }
 
 #[tokio::test]
-async fn int_verify_exec_sends_message_without_error() {
+async fn verify_exec_sends_message_without_error() {
     todo!()
 }
 
 #[tokio::test]
-async fn int_verify_disconnect_is_disconnecting() {
+async fn verify_disconnect_is_disconnecting() {
     todo!("needs test, but after some of the actual queries are built out");
 }
