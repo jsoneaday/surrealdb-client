bb94e000f0cde8cb2971feb03c4fafa68c9f3ae9
David Choi <dharric@live.com>
2022-11-14 13:28:04 -0500

    cleanup tests

--- src/driver/surreal_driver.rs
+++ src/driver/surreal_driver.rs
@@ -1,9 +1,11 @@
 use crate::connection::surreal_ws_conn::{SurrealWsConnection, Method};
 
+#[allow(unused)]
 pub struct SurrealDriver {
     conn: SurrealWsConnection
 }
 
+#[allow(unused)]
 impl SurrealDriver {
     fn new(conn: SurrealWsConnection) -> Self {
         Self {
@@ -11,8 +13,24 @@ impl SurrealDriver {
         }
     }
 
-    async fn ping(&mut self) {        
-        let result = self.conn.rpc(Method::Ping, Vec::new());
-        println!("{:?}", result.await);
+    async fn ping(&mut self) -> Result<tungstenite::Message, tungstenite::Error> {        
+        self.conn.rpc(Method::Ping, Vec::new()).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const HOST: &str = "localhost";
+    const PORT: usize = 8000;
+
+    #[tokio::test]
+    async fn ping_completes_successfully() {
+        let surreal_conn = SurrealWsConnection::new(&HOST, PORT, false);
+        let mut driver = SurrealDriver::new(surreal_conn);
+
+        let result = driver.ping().await;
+        assert!(result.is_ok());
     }
 }
--- tests/surreal_ws_conn_test.rs
+++ tests/surreal_ws_conn_test.rs
@@ -1,20 +1,10 @@
 mod common;
 
-use std::{collections::BTreeMap, borrow::Borrow};
+use std::{collections::BTreeMap};
 
 use surrealdb_client::connection::surreal_ws_conn::Method;
 use surrealdb::sql::Object;
 use common::FIXTURES;
-
-#[tokio::test]
-async fn verify_connect_makes_connection_to_surrealdb() {       
-    let mut fixture = FIXTURES.lock().unwrap();
-    let fixture_items = fixture.instance.as_mut().unwrap();
-    let result = fixture_items.conn.connect().await;
-    
-    assert!(result.is_ok());
-    fixture_items.conn.disconnect().await;
-}
 
 #[tokio::test]
 async fn rpc_ping_completes_successfully() {
@@ -26,9 +16,4 @@ async fn rpc_ping_completes_successfully() {
 
     println!("{:#?}", result.as_ref().unwrap());
     assert!(result.is_ok());
-}
-
-#[tokio::test]
-async fn verify_disconnect_is_disconnecting() {
-    todo!("needs test, but after some of the actual queries are built out");
 }
