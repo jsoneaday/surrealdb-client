9913080ae2bf26a5892f4984a167cdd25d2cc4e3
David Choi <dharric@live.com>
2022-11-18 10:38:26 -0500

    add signin sample

---
+++ examples/sign_in.rs
@@ -0,0 +1,25 @@
+// This example makes a connection to surrealdb and creates an instance of driver in preparation of making calls
+// it then signs into surrealdb with username and password (you must sign in before calling 'use' if you have a password set)
+// use run-docker.sh script on root of library to startup a docker image of surreal
+
+use surrealdb_client::connection::surreal_ws_conn::SurrealWsConnection;
+use surrealdb_client::driver::surreal_driver::SurrealDriver;
+
+pub const HOST: &str = "localhost";
+pub const PORT: usize = 8000;
+pub const USER_NAME: &str = "superduper";
+pub const PASSWORD: &str = "superpass";
+pub const NS: &str = "test";
+pub const DB: &str = "test";
+
+#[tokio::main]
+async fn main() {
+    let mut conn = SurrealWsConnection::new(HOST, PORT, false);
+    let _ = conn.connect().await;
+    let mut driver = SurrealDriver::new(conn);
+
+    let sign_result = driver.sign_in(USER_NAME, PASSWORD).await;
+    println!("{:#?}", sign_result);
+
+    driver.disconnect().await;
+}
